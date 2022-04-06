695eb4c80322900a846cac28d31753c3ab9b9111
Connor Chapin <connorchapin@yahoo.com>
2022-04-06 16:49:53 -0500

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Favicon from "../src/components/Favicon";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
