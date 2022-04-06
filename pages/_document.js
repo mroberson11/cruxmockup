b8609c23cc20fe106c37f7a7203f9df6b88a2848
Connor Chapin <connorchapin@yahoo.com>
2022-04-06 16:46:15 -0500

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Favicon from "../src/components/Favicon";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -29,6 +30,20 @@ export default class MyDocument extends Document {
           <meta name="robots" content="index, follow" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="English" />
+
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-X71DLG71ZJ');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
