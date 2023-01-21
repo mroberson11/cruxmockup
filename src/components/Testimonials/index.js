7d51be4cdeea908002971ba5a5a70b3e9532e468
cruxsoftwaresolutions <cchapin@cruxsoftwaresolutions.com>
2023-01-20 19:11:24 -0600

    fix quotations

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -25,11 +25,11 @@ const Testimonials = () => {
               <Image src={Icon1} alt="Pair Programming" priority={true} />
             </TestimonialsIcon>
             <TestimonialsP>
-              "We are very happy with our website! Professionally done
+              &quot;We are very happy with our website! Professionally done
               throughout the entire process. They were very patient with all our
               staff & teams. They answered all our questions. It&apos;s such a
               peace of mind knowing I don&apos;t have to worry about our
-              website. I highly recommend!"
+              website. I highly recommend!&quot;
             </TestimonialsP>
             <TestimonialsCustomer>Chris Teasley</TestimonialsCustomer>
             <TestimonialsCompany>FBC Crystal Springs</TestimonialsCompany>
@@ -39,8 +39,9 @@ const Testimonials = () => {
               <Image src={Icon2} alt="Sale Generation" priority={true} />
             </TestimonialsIcon>
             <TestimonialsP>
-              "Great designer and website builder. Responds to questions very
-              quickly and delivers a high-quality and professional website."
+              &quot;Great designer and website builder. Responds to questions
+              very quickly and delivers a high-quality and professional
+              website.&quot;
             </TestimonialsP>
             <TestimonialsCustomer>Will Eifling</TestimonialsCustomer>
             <TestimonialsCompany>Delta Dry</TestimonialsCompany>
@@ -50,10 +51,10 @@ const Testimonials = () => {
               <Image src={Icon3} alt="Maintenance" priority={true} />
             </TestimonialsIcon>
             <TestimonialsP>
-              "All content changes, product updates, promotions, upcoming
+              &quot;All content changes, product updates, promotions, upcoming
               events, server management, and the remaining responsibilites
               associated with maintaining a robust E-Commerce website wil be
-              handled for you."
+              handled for you.&quot;
             </TestimonialsP>
             <TestimonialsCustomer>Matthew Roberson</TestimonialsCustomer>
             <TestimonialsCompany>Crux Software Solutions</TestimonialsCompany>
