cd28ba66b8e624ab7240b7a9463848c48c90ec38
cruxsoftwaresolutions <cchapin@cruxsoftwaresolutions.com>
2023-08-01 13:09:43 -0500

    Add Crux BBq to demo page

--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import AltNav from "../AltNav";
 import CruxFastFood from "../../images/DemoImages/fast-food-hero-section.png";
 import CruxPizzeria from "../../images/DemoImages/pizza-hero-section.png";
+import CruxBBQ from "../../images/DemoImages/bbq-hero-section.png";
 import Footer from "../Footer";
 import Image from "next/image";
 import Link from "next/link";
@@ -36,7 +37,7 @@ const Demo = () => {
                 transition={{ delay: 0.1, duration: 0.6 }}
                 animate={animation}
               >
-                Fast Food Demo
+                BBQ Demo
               </InfoSectionHeader>
               <ImgWrap
                 initial={initial}
@@ -47,15 +48,17 @@ const Demo = () => {
                   transition: { duration: 0.2 },
                 }}
               >
-                <Link href="https://56e20ef9aa.nxcli.net" passHref>
-                  <Image
-                    src={CruxFastFood}
-                    alt="Crux Fast Food"
-                    priority={true}
-                  />
-                </Link>
+                <a
+                  href="https://c1347f319e.nxcli.net"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  passHref
+                >
+                  <Image src={CruxBBQ} alt="Crux BBQ" priority={true} />
+                </a>
               </ImgWrap>
             </InfoSection>
+
             <InfoSection>
               <InfoSectionHeader
                 initial={initial}
@@ -73,13 +76,49 @@ const Demo = () => {
                   transition: { duration: 0.2 },
                 }}
               >
-                <Link href="https://e43e510d46.nxcli.net" passHref>
+                <a
+                  href="https://e43e510d46.nxcli.net"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  passHref
+                >
                   <Image
                     src={CruxPizzeria}
                     alt="Crux Pizzeria"
                     priority={true}
                   />
-                </Link>
+                </a>
+              </ImgWrap>
+            </InfoSection>
+            <InfoSection>
+              <InfoSectionHeader
+                initial={initial}
+                transition={{ delay: 0.9, duration: 0.6 }}
+                animate={animation}
+              >
+                Fast Food Demo
+              </InfoSectionHeader>
+              <ImgWrap
+                initial={initial}
+                transition={{ delay: 0.11, duration: 0.6 }}
+                animate={animation}
+                whileHover={{
+                  scale: 1.02,
+                  transition: { duration: 0.2 },
+                }}
+              >
+                <a
+                  href="https://56e20ef9aa.nxcli.net"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  passHref
+                >
+                  <Image
+                    src={CruxFastFood}
+                    alt="Crux Fast Food"
+                    priority={true}
+                  />
+                </a>
               </ImgWrap>
             </InfoSection>
           </DemoContent>
