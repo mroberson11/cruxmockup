fce78d9ecbb8f8c4fda2d4de7f00e558637ed6de
cruxsoftwaresolutions <cchapin@cruxsoftwaresolutions.com>
2025-04-08 18:02:02 -0500

    add more reviews

--- src/components/ReviewCarousel/Data.js
+++ src/components/ReviewCarousel/Data.js
@@ -23,6 +23,30 @@ const reviews = [
       "https://lh3.googleusercontent.com/a/ACg8ocLUYWP8zX4bUqdM7vxzghsv1-vS81OOa6KjWRC_P7Bn=w75-h75-p-rp-mo-br100",
     text: "You can tell they take pride in their work. I couldn't be happier with the results!",
   },
+  {
+    id: 4,
+    name: "Zac Borthwick",
+    job: "Gunsmith",
+    image:
+      "https://lh3.googleusercontent.com/a/ACg8ocJBKQMhwSP_g62e9mCQHn1CJKMQF9455pbHqZzyvSuot1tRAA=w75-h75-p-rp-mo-br100",
+    text: "Absolutely the best. They were able to build me a wonderful website at a great price. They even understood that I'm not the most technology advanced person and was able to explain everything perfectly. I don't have enough room here to express how overly joyed I am with their service",
+  },
+  {
+    id: 5,
+    name: "Curry Black",
+    job: "Fraternity President",
+    image:
+      "https://lh3.googleusercontent.com/a-/ALV-UjXZQVzG0ucNeEWVWhLT_55ED0CWEbtC5_maBTcJQIIAJlRXTt1L=w75-h75-p-rp-mo-br100",
+    text: "Crux Software Solutions totally revamped our website for Sigma Alpha Epsilon. I would absolutely recommend their services for anyone looking to overhaul and modernize their online and digital footprint!",
+  },
+  {
+    id: 6,
+    name: "Charlie Dimino",
+    job: "Author",
+    image:
+      "https://lh3.googleusercontent.com/a/ACg8ocJXnQrL6qELbPZ1Re1qc7DgUBsGxqqrDKYKngch59cDv1T3bA=w45-h45-p-rp-mo-br100",
+    text: "They did a great job! Really love the site they did!",
+  },
 ];
 
 export default reviews;
